b25ad4a4a7f10a2702b10f31903f3a6553f335c0
David <david.njoroge@student.moringaschool.com>
2022-11-27 23:12:56 +0300

    completed active model serialization

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,10 @@ class MoviesController < ApplicationController
     render json: movie
   end
 
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
   private
 
   def render_not_found_response
